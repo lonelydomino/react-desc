68e5b62bfc08d17fd672012c48e17fa6b69c59a1
Milo <zeroeclipse@live.com>
2022-01-15 21:55:10 -0800

    Adding basic arrow function to Productpage.js

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useContext } from 'react'
+import { userParams, Link } from 'react-router-dom'
+import { ShopContext } from '../context/shopContext'
+
+const ProductPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductPage
