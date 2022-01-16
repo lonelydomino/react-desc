ca10b3467e82dde2b5078fa8b28471a3ee08defc
Milo <zeroeclipse@live.com>
2022-01-15 22:00:35 -0800

    Adding router to app.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Home from './pages/Home'
+import { BrowserRouter as Router } from 'react-router-dom'
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Route path="/products/:handle">
+          <ProductPage />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Router>
     </div>
   )
 }
