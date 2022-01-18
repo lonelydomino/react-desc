f28f0ce70edf52486f6482c1aba204943b35b8f9
Milo <zeroeclipse@live.com>
2022-01-17 20:09:47 -0800

    Adding product images to home .js.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 
 const Home = () => {
@@ -11,16 +11,26 @@ const Home = () => {
         fetchAllProducts()
     }, [fetchAllProducts])
 
+    console.log(products)
+
     if (!products) return  <div>Loading...</div>
 
     return (
-        <div>
-            {
-            products.map(product => (
-                <Link to={`/products/${product.handle}`} key={product.title}>{product.title}</Link>
-            ))
-            }
-        </div>
+        <Box>
+            <Grid templateColumns="repeat(3, 1fr)">
+                {
+                products.map(product => ( 
+                    <Link to={`/products/${product.handle}`} key={product.title}>
+                        <Box _hover={{opacity: '80%'}} textAlign="center">
+                            <Image src={product.images[0].src}/>
+                            <Text> {product.title} </Text>
+                            <Text> ${product.variants[0].price}</Text>
+                        </Box>
+                </Link>
+                ))
+                }
+            </Grid>
+        </Box>
     )
 }
 
