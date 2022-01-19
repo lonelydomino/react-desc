489c8021bc158ee23079852e2b8e5c58acf3e967
Milo <zeroeclipse@live.com>
2022-01-18 18:21:56 -0800

    Building out productpage.js.

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
+import { Box, Grid, Image, Text, Button, Heading, Flex, Center } from '@chakra-ui/react'
 
 const ProductPage = () => {
     const { handle } = useParams()
@@ -14,9 +15,17 @@ const ProductPage = () => {
     if(!product.title) return <div>Loading...</div>
 
     return (
-        <div>
-            <Link>{ product.title }</Link>
-        </div>
+        <Box>
+            <Grid templateColumns="repeat(2, 1fr)">
+                <Image src={product.images[0].src} />
+            </Grid>
+            <Box>
+                <Heading>{product.title}</Heading>
+                <Text>{product.variants[0].price}</Text>
+                <Text>{product.description}</Text>
+                <Button onClick={() => addItemToCheckout(product.variants[0].id), 1}>Add To Cart</Button>
+            </Box>
+        </Box>
     )
 }
 
