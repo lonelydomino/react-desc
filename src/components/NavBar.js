4facb5d17633885f3ba2d25231c444d62ad97da5
Milo <zeroeclipse@live.com>
2022-01-19 20:00:40 -0800

    Finishing NavBar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ShopContext } from '../context/shopContext'
-import { Flex, Box, Grid, Text, Image, Icon } from '@chakra-ui/react'
+import { Flex, Box, Grid, Text, Image, Icon, Badge } from '@chakra-ui/react'
 import { useContext } from 'react'
 import { MdMenu, MdShoppingBasket } from 'react-icons/md'
 import { Link } from 'react-router-dom'
@@ -11,12 +11,15 @@ const NavBar = () => {
     const { openCart, openMenu, checkout } = useContext(ShopContext)
 
     return (
-        <Flex backgroundColor="#dcdcdc" flexDir="row" justifyContent="space-between" p="2rem" >
+        <Flex backgroundColor="#dcdcdc" flexDir="row" alignItems="center" justifyContent="space-between" p="2rem" >
             <Icon onClick={() => openMenu()} w={30} h={30} cursor="pointer" fill="black" as={ MdMenu } />
             <Link to="/">
                 <Text w={100}>DESC(Logo placeholder?)</Text>
             </Link>
-            <Icon w={30} h={30} cursor="pointer" fill="black" as={ MdShoppingBasket } onClick={() => openCart()} />
+            <Box>
+                <Icon w={30} h={30} cursor="pointer" fill="black" as={ MdShoppingBasket } onClick={() => openCart()} />
+                <Badge backgroundColor="#FF38BD" borderRadius="50%">{checkout.lineItems?.length}</Badge>
+            </Box>
         </Flex>
     )
 }
