a9b07825fda8de02e2740424b1f356f7326ec944
Milo <zeroeclipse@live.com>
2022-01-19 19:52:33 -0800

    Building out NavMenu.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -8,7 +8,6 @@ import {
     Drawer,
     Button,
     Image,
-    Link,
     DrawerBody,
     DrawerFooter,
     DrawerHeader,
@@ -16,12 +15,31 @@ import {
     DrawerContent,
     DrawerCloseButton,
   } from '@chakra-ui/react'
-  
+  import { Link } from 'react-router-dom'
+
 const NavMenu = () => {
+
+    const { isMenuOpen, closeMenu } = useContext(ShopContext)
+
     return (
-        <div>
-            
-        </div>
+        <Drawer isOpen={isMenuOpen} onClose={closeMenu} placement="left" size="sm">
+            <DrawerOverlay>
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Menu</DrawerHeader>
+                    <DrawerBody>
+                        <VStack>
+                            <Link to="/">About Us</Link>
+                            <Link to="/">Learn More</Link>
+                            <Link to="/">Sustainability</Link>
+                        </VStack>
+                    </DrawerBody>
+                    <DrawerFooter textAlign="center">
+                        <Text w="100%">Copyright DESC</Text>
+                    </DrawerFooter>
+            </DrawerContent>
+            </DrawerOverlay>
+        </Drawer>
     )
 }
 
