37304f9a39f17653743ddf0dcff3e5e869db7a2f
Milo <zeroeclipse@live.com>
2022-01-19 20:37:10 -0800

    Creating main banner.

---
+++ src/components/MainBanner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box, Button, Text, Image, Center } from '@chakra-ui/react'
+
+const MainBanner = () => {
+    return (
+        <Box backgroundColor="gray.300" w="100%" position="relative" h="70vh">
+            <Image src="https://cdn.pixabay.com/photo/2017/08/06/22/52/blouse-2597205_1280.jpg" />
+            
+        </Box>
+    )
+}
+
+export default MainBanner
