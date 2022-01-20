0a8c6e2880119863285f3ab7df5533e2fab88da8
Milo <zeroeclipse@live.com>
2022-01-19 21:03:46 -0800

    Moving ImageWithText in home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,10 @@ import { Box, Grid, Text, Image } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 import MainBanner from '../components/MainBanner'
 import ImageWithText from '../components/ImageWithText'
+import { getByPlaceholderText } from '@testing-library/react'
 
 const Home = () => {
-
+    const placeholderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const {fetchAllProducts, products } = useContext(ShopContext)
 
     useEffect(() => {
@@ -20,6 +21,9 @@ const Home = () => {
     return (
         <Box>
             <MainBanner />
+            <ImageWithText reverse image="https://cdn.pixabay.com/photo/2015/01/21/17/22/shopping-606993_1280.jpg" heading="Heading" text={placeholderText}/>
+            <ImageWithText image="https://cdn.pixabay.com/photo/2020/07/11/16/16/jeans-5394561_1280.jpg" heading="Heading" text={placeholderText}/>
+            <ImageWithText reverse image="https://cdn.pixabay.com/photo/2016/11/19/15/40/clothes-1839935_1280.jpg" heading="Heading" text={placeholderText}/>
             <Grid templateColumns="repeat(3, 1fr)">
                 {
                 products.map(product => ( 
@@ -33,9 +37,7 @@ const Home = () => {
                 ))
                 }
             </Grid>
-            <ImageWithText reverse image="https://cdn.pixabay.com/photo/2015/01/21/17/22/shopping-606993_1280.jpg" heading="Heading"/>
-            <ImageWithText image="https://cdn.pixabay.com/photo/2020/07/11/16/16/jeans-5394561_1280.jpg" heading="Deading"/>
-            <ImageWithText reverse image="https://cdn.pixabay.com/photo/2016/11/19/15/40/clothes-1839935_1280.jpg" heading="Heading"/>
+            
 
         </Box>
     )
