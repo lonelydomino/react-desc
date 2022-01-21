710baed9a307f7c7ce3dbdd378909071b1874b8d
Milo <zeroeclipse@live.com>
2022-01-20 18:19:23 -0800

    Adding netlify.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Box, Grid, Text, Image } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 import MainBanner from '../components/MainBanner'
 import ImageWithText from '../components/ImageWithText'
-import { getByPlaceholderText } from '@testing-library/react'
+
 
 const Home = () => {
     const placeholderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
