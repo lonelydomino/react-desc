a1db8555f2e81df26762043ca8379f2bb4bc1847
Milo <zeroeclipse@live.com>
2022-01-25 21:17:12 -0800

    Adding search bar to navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,21 @@ import { Flex, Box, Text, Icon, Badge } from '@chakra-ui/react'
 import { useContext } from 'react'
 import { MdMenu, MdShoppingBasket } from 'react-icons/md'
 import { Link } from 'react-router-dom'
-
-
+import { Input, InputGroup, InputLeftElement, InputRightAddon } from '@chakra-ui/react'
+import { useState } from 'react'
+import { BiSearchAlt2 } from 'react-icons/bi'
 const NavBar = () => {
 
     const { openCart, openMenu, checkout } = useContext(ShopContext)
+    const [value, setValue] = useState('')
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const handleSubmit = () => {
+        
+    }
 
     return (
         <Flex backgroundColor="black" flexDir="row" alignItems="center" justifyContent="space-between" p="2rem" >
@@ -20,6 +30,10 @@ const NavBar = () => {
                 <Icon w={30} h={30} cursor="pointer" fill="white" as={ MdShoppingBasket } onClick={() => openCart()} />
                 <Badge backgroundColor="#FF38BD" borderRadius="50%">{checkout.lineItems?.length}</Badge>
             </Box>
+            <InputGroup>
+                <InputLeftElement pointerEvents='none' children={<BiSearchAlt2 color='white' />}               />
+                <Input color="white" placeholder='What are you looking for?' value={value} onChange={handleChange} />
+            </InputGroup>
         </Flex>
     )
 }
